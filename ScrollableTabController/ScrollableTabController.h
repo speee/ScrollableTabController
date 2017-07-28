6bb8a7bd2c1f73ddfc9ae03402d7877c7f3512c5
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-07-28 18:04:24 +0900

    Initial commit

---
+++ ScrollableTabController/ScrollableTabController.h
@@ -0,0 +1,19 @@
+//
+//  ScrollableTabController.h
+//  ScrollableTabController
+//
+//  Created by mitsuyoshi.yamazaki on 2017/07/28.
+//  Copyright © 2017年 Mitsuyoshi Yamazaki and Speee, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ScrollableTabController.
+FOUNDATION_EXPORT double ScrollableTabControllerVersionNumber;
+
+//! Project version string for ScrollableTabController.
+FOUNDATION_EXPORT const unsigned char ScrollableTabControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScrollableTabController/PublicHeader.h>
+
+
