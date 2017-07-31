7ade5f66bf9551cd0a86c55730529dc8aa43bae7
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-07-31 16:54:33 +0900

    Add demo target

---
+++ ScrollableTabControllerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollableTabControllerDemo
+//
+//  Created by mitsuyoshi.yamazaki on 2017/07/31.
+//  Copyright © 2017年 Mitsuyoshi Yamazaki and Speee, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
